8a0e6a041ade6c430f4e66a37b150f90fcf36013
Reagan Bodily <reaganbodily@gmail.com>
2022-04-22 15:24:54 -0600

    troubleshooting

--- models/votes.js
+++ models/votes.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Vote extends Model { }
+class Votes extends Model { }
 
-Vote.init(
+Votes.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -36,4 +36,4 @@ Vote.init(
     }
 );
 
-module.exports = Vote;
+module.exports = Votes;
