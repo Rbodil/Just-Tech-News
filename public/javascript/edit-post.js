7bd8eb1459af4beed95f07f61f58270409acb293
Reagan Bodily <reaganbodily@gmail.com>
2022-04-25 19:45:07 -0600

    fixed errors

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,11 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const title = document.querySelector('input[name="post-title"]').value.trim;
 
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
